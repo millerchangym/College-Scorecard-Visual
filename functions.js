c41a3a9ef28b099b1b8456cacb0bb153803ab15a
millerchangym <31051636+millerchangym@users.noreply.github.com>
2017-10-17 22:05:11 -0500

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,4 +1,17 @@
 function updateHeadline() {
+  var newYear = parseInt(d3.select("#year").node().value)
+  
   // update headline
-  d3.select("h2").text(headline + d3.select("#year").node().value);
+  d3.select("h2")
+    .text(headline + 
+	      newYear + 
+		  "-" + 
+		  (newYear + 1) 
+		  );
+}
+
+function updateCircles(){
+	svg.selectAll("circle")
+	   .remove();
+	readCsv(parseInt(d3.select("#year").node().value));
 }
